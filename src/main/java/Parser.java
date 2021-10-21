4b94df761085151d66ed6148f160f7ca40acaed4
Rapha <rapha.israel@live.fr>
2021-10-21 19:39:00 +0200

    add parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,7 +11,26 @@ public class Parser {
 
             //renvoie true s'il y a une autre ligne à lire
             while(scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                //System.out.println(scanner.nextLine());
+                String input = scanner.nextLine();
+                //System.out.print(input);
+
+                String[] arrayInput = input.split("");
+                //System.out.print(arrayInput.length);
+                String[][] result = new String[27][3];
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 27; j++) {
+                        result[j][i] = arrayInput[j];
+                    }
+                }
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 27; j++) {
+                        System.out.print(result[j][i]);
+                    }
+                    System.out.println("");
+                }
+
             }
             scanner.close();
         }
