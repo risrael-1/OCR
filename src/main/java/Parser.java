2421c2fac0df308f1250ec7c5effd30ad6eb64c6
risrael-1 <rapha.israel@live.fr>
2021-10-21 08:21:45 +0200

    Add parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Parser {
+    public static void main(String args[]) {
+        try {
+            // Le fichier d'entrée
+            FileInputStream file = new FileInputStream("C:\\Users\\rapha\\Documents\\test.txt");
+            Scanner scanner = new Scanner(file);
+
+            //renvoie true s'il y a une autre ligne à lire
+            while(scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
